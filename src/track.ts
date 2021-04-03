b9c64b928a2eeafb441c6a118f3e6ccbe85e0ed1
hakuna-tata <610578197@qq.com>
2021-04-04 02:21:59 +0800

    fix(name): 修复track文件名

---
+++ src/track.ts
@@ -0,0 +1,23 @@
+import pageInstance from "./page";
+import Subscriber from "./subscriber";
+import FormatData from "./pipeline/formatData";
+import BrowserDebug from "./pipeline/browserDebug";
+
+class Track {
+    private subscriber: Subscriber;
+    private formatData: FormatData;
+    private browserDebug: BrowserDebug;
+
+    init(): void {
+        pageInstance.init();
+
+        this.subscriber = new Subscriber();
+        this.formatData = new FormatData();
+        this.browserDebug = new BrowserDebug();
+
+        this.subscriber.pipe(this.formatData).pipe(this.browserDebug);
+        this.subscriber.subscribe();
+    }
+}
+
+export default Track;
