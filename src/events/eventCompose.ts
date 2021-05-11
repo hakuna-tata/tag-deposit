51c1c8d68882cedce145681c0b36f223811c9c59
hakuna-tata <610578197@qq.com>
2021-05-12 01:23:21 +0800

    feat(info): 标注错误信息精准提示

--- src/events/eventCompose.ts
+++ src/events/eventCompose.ts
@@ -36,8 +36,8 @@ export default class EventCompose {
 
     private parseNodePath(): TdNodePath[] {
         const tdNodePath: TdNodePath[] = this.reduceEventTarget(this.event);
-        // 寻找离目标节点最近的 appid
-        return this.fixTdNodePath(tdNodePath, "appid");
+        // 寻找至目标节点最近的 appid
+        return this.traceTdNodePath(tdNodePath, "appid");
     }
 
     private reduceEventTarget(e: Event): TdNodePath[] {
@@ -84,16 +84,23 @@ export default class EventCompose {
         return Object.keys(node.attrs).length > 0 ? node : null;
     }
 
-    private fixTdNodePath(tdNodePath: TdNodePath[], fixId: string): TdNodePath[] {
+    private traceTdNodePath(tdNodePath: TdNodePath[], traceId: string): TdNodePath[] {
+        const targetInfo: TdNodePath = tdNodePath[0];
+
         const index = tdNodePath.findIndex((node: TdNodePath) => {
-            return node.attrs[fixId];
+            return node.attrs[traceId];
         });
 
         const nodes: TdNodePath[] = tdNodePath.slice(0, index + 1);
 
         if (!nodes.length) {
-            const { attrs } = tdNodePath[tdNodePath.length - 1];
-            Logger.warn(`td-pageid: ${attrs.pageid} 没有对应的 td-${fixId}`);
+            const curId = VALID_VTNODE.find((attr) => {
+                return Object.keys(targetInfo.attrs).includes(attr);
+            });
+
+            Logger.warn(`td-${curId}: ${targetInfo.attrs[curId]} 没有对应的 td-${traceId}`);
+
+            return [];
         }
 
         return nodes;
@@ -120,8 +127,8 @@ export default class EventCompose {
         reportItem.targetEl = this.targetEl;
         reportItem.app_id = this.appInfo.attrs.appid;
 
-        // 寻找离目标节点最近的 pageid
-        const pageNodePath: TdNodePath[] = this.fixTdNodePath(tdNodePath, "pageid");
+        // 寻找至目标节点最近的 pageid
+        const pageNodePath: TdNodePath[] = this.traceTdNodePath(tdNodePath, "pageid");
 
         // page 维度
         if (pageNodePath.length === 1) {
@@ -132,17 +139,29 @@ export default class EventCompose {
             // module 维度
         } else if (pageNodePath.length === 2) {
             reportItem.page_id = pageNodePath[1].attrs.pageid;
-            reportItem.module_id = pageNodePath[0].attrs.moduleid;
+            if (pageNodePath[0].attrs.moduleid) {
+                reportItem.module_id = pageNodePath[0].attrs.moduleid;
 
-            reportItem.level = ACTION_LEVEL.MODULE;
-
+                reportItem.level = ACTION_LEVEL.MODULE;
+            } else {
+                const targetInfo: TdNodePath = pageNodePath[0];
+                Logger.warn(`td-itemid: ${targetInfo.attrs.itemid} 没有对应的 td-moduleid`);
+            }
             // item 维度
         } else if (pageNodePath.length === 3) {
             reportItem.page_id = pageNodePath[2].attrs.pageid;
-            reportItem.module_id = pageNodePath[1].attrs.moduleid;
-            reportItem.item_id = pageNodePath[0].attrs.itemid;
 
-            reportItem.level = ACTION_LEVEL.ITEM;
+            if (pageNodePath[0].attrs.itemid && pageNodePath[1].attrs.moduleid) {
+                reportItem.module_id = pageNodePath[1].attrs.moduleid;
+                reportItem.item_id = pageNodePath[0].attrs.itemid;
+
+                reportItem.level = ACTION_LEVEL.ITEM;
+            } else {
+                // TODO (detail info)
+                Logger.warn("禁止 itemid 以及 moduleid 自身嵌套，或者互相嵌套");
+            }
+        } else if (pageNodePath.length > 3) {
+            Logger.warn("禁止 itemid 以及 moduleid 自身嵌套，或者互相嵌套");
         }
 
         return reportItem;
